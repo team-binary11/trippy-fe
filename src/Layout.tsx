e5069f45bbbcdad74618a8e1ce8abf2e7f58b239
Lalit Kumar <lalit292929@gmail.com>
2020-10-03 13:07:57 +0530

    addition of calender and autocomplete input

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+import ICalender from './lib/shared/inputs/calender/ICalender';
+import IAutoComplete from './lib/shared/inputs/autocomplete/IAutoComplete';
 
 const Layout = () => (
   <h1>
     Hi world
+    <ICalender />
+    <IAutoComplete />
   </h1>
 )
 
