9953c04eaa1db3f13ddcc1ce93697d6b2298306e
Deepanshu Jain <deepanshujain1234@live.com>
2020-10-04 14:06:58 +0530

    Updates

---
+++ precache-manifest.7d1497d76987cfaa0e43cbbaa94e5001.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f562d01abe8d537f9a08a2a22d242016",
+    "url": "/trippy-fe/index.html"
+  },
+  {
+    "revision": "455ffbe27929233799e5",
+    "url": "/trippy-fe/static/css/2.27b0fa54.chunk.css"
+  },
+  {
+    "revision": "37df05002f46b3cc1406",
+    "url": "/trippy-fe/static/css/main.d9e4c816.chunk.css"
+  },
+  {
+    "revision": "455ffbe27929233799e5",
+    "url": "/trippy-fe/static/js/2.d8d643fa.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/trippy-fe/static/js/2.d8d643fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37df05002f46b3cc1406",
+    "url": "/trippy-fe/static/js/main.501f1fca.chunk.js"
+  },
+  {
+    "revision": "3fd4aafd94ffd9e0ef94",
+    "url": "/trippy-fe/static/js/runtime-main.8f384830.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/trippy-fe/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "159263bf4976843830365d52d1f94fba",
+    "url": "/trippy-fe/static/media/primeicons.159263bf.eot"
+  },
+  {
+    "revision": "2e3a6b48408add8bb604d0b4e820a3b5",
+    "url": "/trippy-fe/static/media/primeicons.2e3a6b48.ttf"
+  },
+  {
+    "revision": "4e915114c908962a6ff2d505c2a4816b",
+    "url": "/trippy-fe/static/media/primeicons.4e915114.svg"
+  },
+  {
+    "revision": "79432094e4838b36d974b9d8c57d22e2",
+    "url": "/trippy-fe/static/media/primeicons.79432094.woff"
+  }
+]);
